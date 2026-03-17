f6484ec3deaeb7b58380be1cdaeaf56b22e9c0b2
jgallego97 <1505163@alu.murciaeduca.es>
2026-03-17 18:00:02 +0100

    Añade programa a la nueva rama Practica4_1

--- Principal.java
+++ Principal.java
@@ -1,5 +1,20 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Principal{
 	public static void main(String[] args){
 		System.out.println("Hola mundo");
+		horaLocal();
+		
+
 	}
+	public static void horaLocal(){
+		LocalTime horaActual = LocalTime.now();
+        System.out.println("Hora local: " + horaActual);
+
+        // Dar formato a la hora (HH:mm:ss)
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println("Hora formateada: " + horaActual.format(formatter));
+	}
+
 }
